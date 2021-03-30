0b1027f0182c639b74e229bef91bf23717237d2a
Sheltorious-Rex <44091958+Sheltorious-Rex@users.noreply.github.com>
2021-03-30 00:17:35 -0600

    Added populate_table.sql

---
+++ populate_tables.sql
@@ -0,0 +1,107 @@
+INSERT INTO SI_COMPONENT
+VALUES ();
+
+INSERT INTO ARTICLE
+VALUES ();
+
+INSERT INTO ARTIFACT
+VALUES ();
+
+INSERT INTO ARTWORK
+VALUES (0,'Bonehalisa', 'Lovely colors depicting a chick and a flower', 'Fhil','Harper', '1640-06-15', 'Painting', 'Bonehalisa/si.com');
+VALUES (1,'Love Hammer', 'Lovely colors depicting a flower and a stick', 'Fhil','Harper', '1641-06-11', 'Painting', 'lovehammer/si.com');
+VALUES (2,'Sticky Chops', 'Lunch with an uncle', 'Fhil','Harper', '1740-02-1', 'Painting', 'stickychops/si.com');
+VALUES (3,'Swan neck', 'A picture of a goose neck', 'Dana','Swamble', '1940-08-15', 'Painting', 'Bonehalisa/si.com');
+VALUES (4,'Bonehalisa', 'Lovely colors depicting a chick and a flower', 'Fhil','Harper', '1640-06-15', 'Painting', 'Bonehalisa/si.com');
+VALUES (5,'Bonehalisa', 'Lovely colors depicting a chick and a flower', 'Fhil','Harper', '1640-06-15', 'Painting', 'Bonehalisa/si.com');
+VALUES (6,'Bonehalisa', 'Lovely colors depicting a chick and a flower', 'Fhil','Harper', '1640-06-15', 'Painting', 'Bonehalisa/si.com');
+VALUES (7,'Bonehalisa', 'Lovely colors depicting a chick and a flower', 'Fhil','Harper', '1640-06-15', 'Painting', 'Bonehalisa/si.com');
+INSERT INTO BIOPARK
+VALUES ();
+
+INSERT INTO CULTURAL_CENTER
+VALUES ();
+
+INSERT INTO CULTURAL_RESOURCE
+VALUES ();
+
+INSERT INTO EDUCATION_CENTER
+VALUES ();
+
+INSERT INTO EVENT
+VALUES ();
+
+INSERT INTO EXHIBITION
+VALUES ();
+
+INSERT INTO FAUNA
+VALUES ();
+
+INSERT INTO FILM
+VALUES ();
+
+INSERT INTO FLORA
+VALUES ();
+
+INSERT INTO GIFT
+VALUES ();
+
+INSERT INTO INITIATIVE
+VALUES ();
+
+INSERT INTO MEMBER_LEVEL
+VALUES ();
+
+INSERT INTO MACRO_LOCALE
+VALUES ();
+
+INSERT INTO MICRO_LOCALE
+VALUES ();
+
+INSERT INTO MEMBER
+VALUES ();
+
+INSERT INTO MUSEUM
+VALUES ();
+
+INSERT INTO OBJECT
+VALUES ();
+
+INSERT INTO ORGANISM
+VALUES ();
+
+INSERT INTO RECORD
+VALUES ();
+
+INSERT INTO RESEARCH_CENTER
+VALUES ();
+
+INSERT INTO PROJECT
+VALUES ();
+
+INSERT INTO THEATER
+VALUES ();
+
+INSERT INTO display
+VALUES ();
+
+INSERT INTO donate
+VALUES ();
+
+INSERT INTO engage
+VALUES ();
+
+INSERT INTO hosts
+VALUES ();
+
+INSERT INTO researches
+VALUES ();
+
+INSERT INTO show
+VALUES ();
+
+INSERT INTO use
+VALUES ();
+
+INSERT INTO utilize
+VALUES ();
